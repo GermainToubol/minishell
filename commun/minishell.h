8b97ffa9e368fcef8a688e0ccfe6ae965b41b5ab
fmauguin <fmauguin@fmauguin-ubuntu.home>
2022-06-09 18:38:42 +0200

    new struct minishell

--- commun/minishell.h
+++ commun/minishell.h
@@ -3,12 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gtoubol <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: fmauguin <fmauguin@student.42.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 17:14:28 by gtoubol           #+#    #+#             */
-/*   Updated: 2022/06/08 17:46:39 by gtoubol          ###   ########.fr       */
+/*   Updated: 2022/06/09 18:37:54 by fmauguin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
@@ -17,18 +18,24 @@
   no output or pipe: NULL
   no errput: NULL
   bad cmd : ->path_exec=NULL and ->cmd=cmd
+  out : stdout ou stderr 1 ou 2
 */
 
 typedef struct s_cmd
 {
-	char	*input;
-	char	*output;
-	char	*errput;
-	char	*path_exec;
-	char	**cmd;
+	char		*input;
+	char		*path_exec;
+	char		**cmd;
+	t_redirect	**outputs;
+}	t_cmd;
+
+typedef struct s_redirect
+{
+	char	*file;
+	int		out;
 	int		heredoc;
 	int		append;
-}	t_cmd;
+}	t_redirect;
 
 # define IS_HEREDOC 1
 # define NO_HEREDOC 0
