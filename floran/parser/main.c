02c138baaee10266c6e3086528dcac791c00ff03
fmauguin <fmauguin@fmauguin-ubuntu.home>
2022-06-15 15:30:32 +0200

    Revert "fix error atoll"
    This reverts commit fcbf505a838f10eef93b5077fb2e1d0300d16eda.

--- floran/parser/main.c
+++ floran/parser/main.c
@@ -6,7 +6,7 @@
 /*   By: fmauguin <fmauguin@student.42.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 00:58:06 by fmauguin          #+#    #+#             */
-/*   Updated: 2022/06/15 13:28:47 by fmauguin         ###   ########.fr       */
+/*   Updated: 2022/06/15 01:01:28 by fmauguin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,9 @@ int	main(void)
 		line = readline("\x1b[33mMINISHELL$> \x1b[0m");
 		if (!line)
 			return (1);
-		if (!lexer(line, &tokens))
-			print_lexer(line, &tokens);
+		if (lexer(line, &tokens))
+			return (free(line), 1);
+		print_lexer(line, &tokens);
 		free(line);
 		free_lxm(tokens.tokens, tokens.size);
 	}
