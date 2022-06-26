8612454a38c2217a6d57b71a977e3608099a9c2c
Floran Mauguin <fmauguin.pro@gmail.com>
2022-06-26 19:19:37 +0200

    norme + if $ is null in quotes

--- commun/srcs/expand/quotes/expand_quotes.c
+++ commun/srcs/expand/quotes/expand_quotes.c
@@ -6,7 +6,7 @@
 /*   By: fmauguin <fmauguin@student.42.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/26 16:46:09 by fmauguin          #+#    #+#             */
-/*   Updated: 2022/06/26 19:04:12 by fmauguin         ###   ########.fr       */
+/*   Updated: 2022/06/26 19:18:59 by fmauguin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,23 @@ size_t	i_matching_quote(const char *cmd, size_t start)
 	return (0);
 }
 
+static int	rec_do_quotes(const char *cmd, size_t *i,
+				char **ret, char *tmp)
+{
+	char	*tmp2;
+
+	if (cmd[*i] == '\'' && strjoin_custom(ret, tmp))
+		return (1);
+	else if (cmd[*i] == '"')
+	{
+		tmp2 = expand_var(tmp);
+		free(tmp);
+		if (tmp2 && strjoin_custom(ret, tmp2))
+			return (1);
+	}
+	return (0);
+}
+
 static int	rec_quote_content(const char *cmd, size_t *i, char **ret)
 {
 	size_t	start;
@@ -42,14 +59,8 @@ static int	rec_quote_content(const char *cmd, size_t *i, char **ret)
 		tmp = ft_substr(cmd, start, *i - start);
 		if (!tmp)
 			return (display_error("Error allocation\n", 0), 1);
-		if (cmd[*i] == '\'' && strjoin_custom(ret, tmp))
+		if (rec_do_quotes(cmd, i, ret, tmp))
 			return (1);
-		else if (cmd[*i] == '"')
-		{
-			if (strjoin_custom(ret, expand_var(tmp)))
-				return (free(tmp), 1);
-			free(tmp);
-		}
 	}
 	if (rec_quote(cmd, *i + 1, ret))
 		return (1);
