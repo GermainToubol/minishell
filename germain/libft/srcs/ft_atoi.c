97be3a1a3f44aaa540d12975f8fdeb9ba420a76b
Germain Toubol <germain.toubol.pro@gmail.com>
2022-06-30 10:48:14 +0200

    Libft Edit
    Change ft_atoi to protect from overflow / Underflow
    Update Makefile to pass on cc

--- germain/libft/srcs/ft_atoi.c
+++ germain/libft/srcs/ft_atoi.c
@@ -9,6 +9,7 @@
 /*   Updated: 2022/05/09 09:50:11 by gtoubol          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <limits.h>
 #include "libft.h"
 
 static int	ft_isspace(char c);
@@ -32,6 +33,8 @@ int	ft_atoi(const char *s)
 	}
 	while (s[i] >= '0' && s[i] <= '9')
 	{
+		if ((INT_MAX - s[i] + '0') / 10 < n )
+			return (0);
 		n = 10 * n + (s[i] - '0');
 		i++;
 	}
