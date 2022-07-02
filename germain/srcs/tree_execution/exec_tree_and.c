6f9a4c3344ab76f65389d32f87e4a7de3c87fc67
Germain Toubol <germain.toubol.pro@gmail.com>
2022-07-02 21:57:38 +0200

    Solved bug in subshells
    Remove inter pipes in logical operation and bind subshells directly to stdin/stdout

--- germain/srcs/tree_execution/exec_tree_and.c
+++ germain/srcs/tree_execution/exec_tree_and.c
@@ -17,44 +17,20 @@
 #include "minishell.h"
 #include "g_minishell.h"
 
-static void	close_pipe(int *pfd);
-
 pid_t	exec_tree_and(t_astree *node, int *pipe_in, int *pipe_out,
 			t_clean *cleanable)
 {
 	pid_t	pid;
-	int		tmp[2] = {-2, -2};
 	int		n;
 
-	tmp[0] = -2;
-	tmp[1] = -2;
-	if (pipe_in[0] != -2)
-		//tmp[0] = dup(pipe_in[0]);
-		tmp[0] = dup2(pipe_in[0], 505);
-	cleanable_add_pipe(cleanable, tmp);
 	n = count_wait_tree(node->left, cleanable->depth);
 	pid = exec_tree(node->left, pipe_in, pipe_out, cleanable);
-	close_pipe(pipe_in);
 	wait_all(n, pid);
-	pipe_in[0] = tmp[0];
-	//cleanable->n_pipes -= 1;
 	if (get_status() == 0 && get_exit_state() == 0 && is_interupted() == 0)
 	{
 		n = count_wait_tree(node->right, cleanable->depth);
 		pid = exec_tree(node->right, pipe_in, pipe_out, cleanable);
-		close_pipe(pipe_in);
 		wait_all(n, pid);
 	}
-	else
-		close(tmp[0]);
-	close_pipe(pipe_out);
 	return (pid);
-}
-
-static void	close_pipe(int *pfd)
-{
-	if (pfd[0] != -2)
-		close(pfd[0]);
-	if (pfd[1] != -2)
-		close(pfd[1]);
 }
--- germain/srcs/tree_execution/exec_tree_or.c
+++ germain/srcs/tree_execution/exec_tree_or.c
@@ -17,40 +17,20 @@
 #include "minishell.h"
 #include "g_minishell.h"
 
-static void	close_pipe(int *pfd);
-
 pid_t	exec_tree_or(t_astree *node, int *pipe_in, int *pipe_out,
 			t_clean *cleanable)
 {
 	pid_t	pid;
-	int		tmp[2];
 	int		n;
 
-	tmp[0] = -2;
-	tmp[1] = -2;
-	if (pipe_in[0] != -2)
-		tmp[0] = dup(pipe_in[0]);
-	cleanable_add_pipe(cleanable, tmp);
 	n = count_wait_tree(node->left, cleanable->depth);
 	pid = exec_tree(node->left, pipe_in, pipe_out, cleanable);
-	close_pipe(pipe_in);
-	pipe_in[0] = tmp[0];
 	wait_all(n, pid);
 	if (get_status() != 0 && get_exit_state() == 0 && is_interupted() == 0)
 	{
 		n = count_wait_tree(node->right, cleanable->depth);
 		pid = exec_tree(node->right, pipe_in, pipe_out, cleanable);
-		close(tmp[0]);
 		wait_all(n, pid);
 	}
-	else
-		close(tmp[0]);
-	close_pipe(pipe_out);
-	return (1);
-}
-
-static void	close_pipe(int *pfd)
-{
-	close(pfd[0]);
-	close(pfd[1]);
+	return (pid);
 }
