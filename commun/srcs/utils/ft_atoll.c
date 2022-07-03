702e192edbc67073ab2ab6137cfaea5b69d968bd
Germain Toubol <germain.toubol.pro@gmail.com>
2022-07-03 17:28:39 +0200

    update ATOLL: still wrong

--- commun/srcs/utils/ft_atoll.c
+++ commun/srcs/utils/ft_atoll.c
@@ -19,7 +19,6 @@ int	ft_atoll(char *s, long long *n)
 
 	*n = 0;
 	minus = 1;
-	n = 0;
 	if (!s)
 		return (0);
 	if (*s == '+' || *s == '-')
@@ -30,12 +29,12 @@ int	ft_atoll(char *s, long long *n)
 	}
 	while (ft_isdigit(*s))
 	{
-		if (minus > 0 && (LLONG_MAX - *s + '0') / 10 < n)
+		if (minus > 0 && (LLONG_MAX - *s + '0') / 10 < *n)
 			return (1);
-		if (minus < 0 && (LLONG_MIN + *s - '0') / 10 > -n)
+		if (minus < 0 && (LLONG_MIN + *s - '0') / 10 > -*n)
 			return (1);
-		n *= 10;
-		n += *s - '0';
+		*n *= 10;
+		*n += *s - '0';
 		s++;
 	}
 	*n *= minus;
