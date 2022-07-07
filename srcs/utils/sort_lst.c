fc89310a1b0280cb365e892e46386e77063115db
Floran Mauguin <fmauguin.pro@gmail.com>
2022-07-07 23:20:48 +0200

    custom strcmp in sort_lst

--- srcs/utils/sort_lst.c
+++ srcs/utils/sort_lst.c
@@ -6,7 +6,7 @@
 /*   By: fmauguin <fmauguin@student.42.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 19:01:24 by fmauguin          #+#    #+#             */
-/*   Updated: 2022/07/06 23:57:55 by fmauguin         ###   ########.fr       */
+/*   Updated: 2022/07/07 23:19:57 by fmauguin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,23 @@
 
 static void	swap_data(void **a, void **b);
 static void	quicksort_lst(t_list **lst, size_t len);
+
+static char	to_lower(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (c + 32);
+	return (c);
+}
+
+static int	ft_strcmp_custom(const char *s1, const char *s2)
+{
+	while (*s1 && *s2 && to_lower(*s1) == to_lower(*s2))
+	{
+		s1++;
+		s2++;
+	}
+	return ((unsigned char)to_lower(*s1) - (unsigned char )to_lower(*s2));
+}
 
 void	sort_list(t_list **lst)
 {
@@ -38,7 +55,7 @@ static void	quicksort_lst(t_list **lst, size_t len)
 	pivot = 0;
 	while (i < len)
 	{
-		if (ft_strcmp(ft_list_at(*lst, i)->content,
+		if (ft_strcmp_custom(ft_list_at(*lst, i)->content,
 				ft_list_at(*lst, len - 1)->content) < 0)
 		{
 			swap_data(&(ft_list_at(*lst, i)->content),
